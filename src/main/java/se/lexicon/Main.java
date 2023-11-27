b043c04b858c801a89b9dfe753c219780340d5d4
Goran M <goranmuminovic@gmail.com>
2023-11-27 23:42:20 +0100

    partial

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,7 +1,16 @@
 package se.lexicon;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] numbers = {11,23,39};
+        System.out.println(Arrays.toString(numbers));
+
+
     }
+
+    public static String[] indexOf
 }
