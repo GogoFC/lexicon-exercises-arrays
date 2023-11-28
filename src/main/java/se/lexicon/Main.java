18dbbb823bb5ae5a41c9992369ef1b724bbad7b5
Goran M <goranmuminovic@gmail.com>
2023-11-28 21:06:54 +0100

    iterator

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -2,6 +2,7 @@ package se.lexicon;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
@@ -28,9 +29,21 @@ public class Main {
         countryCity[2][1] = "Louisville";
         countryCity[3][0] = "Australia";
         countryCity[3][1] = "Sydney";
-        System.out.println(Arrays.deepToString(countryCity));
-        System.out.println(Arrays.deepToString(countryCity).replace("], [", "],\n ["));
-        System.out.println(Arrays.asList(countryCity));
+
+        System.out.println(Arrays.deepToString(countryCity) + "\n");
+        System.out.println(Arrays.deepToString(countryCity).replace("], [", "],\n [") + "\n");
+
+        Iterator<String[]> itrC = Arrays.asList(countryCity).iterator();
+        while (itrC.hasNext()) {
+            System.out.println(Arrays.asList(itrC.next()));
+        }
+
+        /*
+        for (String[] strings : Arrays.asList(countryCity)) {
+            System.out.println(strings);
+        }
+        */
+        //System.out.println(Arrays.asList(countryCity));
 
 
 
